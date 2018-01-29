ec89eb30ca1b951f5b391bed7c82736499532dc0
Zander Cittadin <zcittadin@gmail.com>
2018-01-29 16:22:59 -0200

    Correção no CORS filter

--- src/main/java/zan/ind/productionmanager/security/WebSecurityConfig.java
+++ src/main/java/zan/ind/productionmanager/security/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		configuration.setAllowCredentials(true);
 		// setAllowedHeaders is important! Without it, OPTIONS preflight request
 		// will fail with 403 Invalid CORS request
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		configuration.setAllowedHeaders(Arrays.asList("Origin", "Authorization", "Cache-Control", "Content-Type"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
